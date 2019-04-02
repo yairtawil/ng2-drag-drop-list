44bdc8ee59c8614b64228bc2baa4b3db4df9baf5
YairTawil <yairtawil@gmail.com>
2019-04-02 08:48:12 +0300

    fix: travis

--- projects/ng-drag-drop-list/src/lib/drag-drop-list.directive.spec.ts
+++ projects/ng-drag-drop-list/src/lib/drag-drop-list.directive.spec.ts
@@ -2,7 +2,7 @@ import { DragDropListDirective } from './drag-drop-list.directive';
 
 describe('DragDropListDirective', () => {
   it('should create an instance', () => {
-    const directive = new DragDropListDirective(null);
+    const directive = new DragDropListDirective({ nativeElement: null });
     expect(directive).toBeTruthy();
   });
 });
